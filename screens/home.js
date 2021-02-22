8260e539b272fdc3aff6bb85d17a76a2a5c636b6
sanimridha <sanimridha307@gmail.com>
2021-02-22 14:48:00 +0600

    top navigation bar completed

--- screens/home.js
+++ screens/home.js
@@ -1,10 +1,44 @@
 import React from 'react';
-import {StyleSheet, Text, View} from 'react-native';
+import {StyleSheet, Text, TouchableOpacity, View, Image} from 'react-native';
 import {COLORS, SIZES, FONTS, icons} from '../constants';
 const home = () => {
+  const renderNavBar = () => {
+    return (
+      <View
+        style={{
+          flexDirection: 'row',
+          height: 40,
+          justifyContent: 'space-between',
+          alignItems: 'flex-end',
+          paddingHorizontal: SIZES.padding / 2,
+          backgroundColor: COLORS.white,
+        }}>
+        <TouchableOpacity
+          style={{justifyContent: 'center', width: 50}}
+          onPress={() => console.log('Back')}>
+          <Image
+            source={icons.back_arrow}
+            resizeMode={'contain'}
+            style={{width: 30, height: 30, tintColor: COLORS.primary}}
+          />
+        </TouchableOpacity>
+        <TouchableOpacity
+          style={{justifyContent: 'center', width: 50, alignItems: 'flex-end'}}
+          onPress={() => console.log('More')}>
+          <Image
+            source={icons.more}
+            resizeMode={'contain'}
+            style={{width: 30, height: 30, tintColor: COLORS.primary}}
+          />
+        </TouchableOpacity>
+      </View>
+    );
+  };
+
   return (
-    <View>
-      <Text>Home</Text>
+    <View style={{flex: 1, backgroundColor: COLORS.lightGray2}}>
+      {/* Nav bar Section */}
+      {renderNavBar()}
     </View>
   );
 };
