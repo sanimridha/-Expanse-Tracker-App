2c9e23845029fb4d99c2716707e77f335571dab3
sanimridha <sanimridha307@gmail.com>
2021-02-22 12:05:21 +0600

    Screen init

---
+++ screens/home.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import {StyleSheet, Text, View} from 'react-native';
+import {COLORS, SIZES, FONTS, icons} from '../constants';
+const home = () => {
+  return (
+    <View>
+      <Text>Home</Text>
+    </View>
+  );
+};
+
+export default home;
+
+const styles = StyleSheet.create({});
