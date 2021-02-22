075e9da705305d7f7db8ab58d843add1fb60fc83
sanimridha <sanimridha307@gmail.com>
2021-02-22 11:42:11 +0600

    added SIZES into theme

--- constants/theme.js
+++ constants/theme.js
@@ -21,3 +21,26 @@ export const COLORS = {
   purple: '#8e44ad',
   red: '#FF0000',
 };
+export const SIZES = {
+  // global sizes
+  base: 8,
+  font: 14,
+  radius: 12,
+  padding: 24,
+  padding2: 36,
+
+  // font sizes
+  largeTitle: 50,
+  h1: 30,
+  h2: 22,
+  h3: 16,
+  h4: 14,
+  body1: 30,
+  body2: 20,
+  body3: 16,
+  body4: 14,
+
+  // app dimensions
+  width,
+  height,
+};
